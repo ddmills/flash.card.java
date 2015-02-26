4aa4ff5185447b7a5b76c788f02e4c4170d14741
dmills <dmills@iastate.edu>
2015-02-26 14:26:54 -0600

    seperated view out a little

--- src/flash/card/java/view/Prompt.java
+++ src/flash/card/java/view/Prompt.java
@@ -5,27 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Prompt {
+    private static void print(String line) {
+        System.out.println(line);
+    }
+    
+    private static String read(BufferedReader br) {
+        try {
+            return br.readLine();
+        } catch (IOException io) {
+            // invalid input?
+            io.printStackTrace();
+            return null;
+        }
+    }
+    
     public static void main (String args[]) {
         boolean running;
         String input;
         BufferedReader br;
         
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
         running = true;
         
         do {
-            try {
-                br = new BufferedReader(new InputStreamReader(System.in));
-
-                while ((input = br.readLine()) != null) {
-                    System.out.println(input);
-                }
-                
-            } catch (IOException io) {
-                // invalid input?
-                io.printStackTrace();
+            input = read(br);
+            print(input);
+            
+            if (input.equals("exit")) {
+                print("goodbye!");
+                running = false;
             }
-        
         } while (running);
-        
     }
 }
