0b0c776ce67b093b6efee24d6209b881626dabf7
dmills <dmills@iastate.edu>
2015-02-26 14:58:37 -0600

    simple inputs

--- src/flash/card/java/view/Prompt.java
+++ src/flash/card/java/view/Prompt.java
@@ -4,8 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import flash.card.java.controller.SchoolController;
+
 public class Prompt {
+    private static void print(int line) {
+        System.out.print(line);
+    }
+    
     private static void print(String line) {
+        System.out.print(line);
+    }
+    
+    private static void println(int line) {
+        System.out.println(line);
+    }
+    
+    private static void println(String line) {
         System.out.println(line);
     }
     
@@ -19,22 +33,49 @@ public class Prompt {
         }
     }
     
+    private static int readInt(BufferedReader br) {
+        while (true) {
+            try {
+                String inpt = read(br);
+                int ret = Integer.parseInt(inpt);
+                return ret;
+            } catch (NumberFormatException e) {
+                println("You must input an integer.");
+            }
+        }
+    }
+    
     public static void main (String args[]) {
         boolean running;
         String input;
         BufferedReader br;
         
         
+        SchoolController school = new SchoolController();
+        
         br = new BufferedReader(new InputStreamReader(System.in));
         running = true;
         
         do {
+            print("> ");
             input = read(br);
-            print(input);
+            println(input);
             
-            if (input.equals("exit")) {
-                print("goodbye!");
+            switch (input) {
+                
+            case "exit":
                 running = false;
+                println("goodbye!");
+                break;
+            case "login":
+                print("Please enter username: ");
+                String name = read(br);
+                print("Please enter password: ");
+                String pass = read(br);
+                println("you input: " + name + " " + pass);
+                break;
+            default:
+                println("Command not recognized");
             }
         } while (running);
     }
