0a8c705908755b8dfb629185286c5b45913c8e87
dmills <dmills@iastate.edu>
2015-02-26 14:34:45 -0600

    added current user singleton

---
+++ src/flash/card/java/model/CurrentUser.java
@@ -0,0 +1,15 @@
+package flash.card.java.model;
+
+/*
+ * CurrentUser Singleton
+ */
+public class CurrentUser {
+    private static CurrentUser instance = null;
+
+    public static CurrentUser getInstance() {
+        if (instance == null) {
+            instance = new CurrentUser();
+        }
+        return instance;
+    }
+}
