bf6bf2995f617b0e4f53e7f84e76ae236858487b
Trey <treye22@gmail.com>
2015-02-26 14:04:38 -0600

    Added SchoolController and Interface

--- src/flash/card/java/controller/SchoolController.java
+++ src/flash/card/java/controller/SchoolController.java
@@ -1,5 +1,77 @@
 package flash.card.java.controller;
 
-public class SchoolController {
+public class SchoolController implements SchoolControllerInterface{
+
+	@Override
+	public int createTeacher(String name, String pass) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addStudent(String name, String pass) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean login(int userID, String pass) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int createDeck(String title, String description) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addCard(String front, String back, int deckID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean removeCard(int cardID, int deckID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int createQuiz(String title, String description, int deckID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean addAllStudentsToQuiz(String quizID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addStudentToQuiz(String studentID, String quizID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAllStudentsFromQuiz(String quizID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeStudentFromQuiz(String studentID, String quizID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
---
+++ src/flash/card/java/controller/SchoolControllerInterface.java
@@ -0,0 +1,18 @@
+package flash.card.java.controller;
+
+public interface SchoolControllerInterface {
+	int createTeacher (String name, String pass);
+	int addStudent (String name, String pass);
+	boolean login (int userID, String pass);
+	void logout ();
+	
+	int createDeck (String title, String description);
+	int addCard (String front, String back, int deckID);
+	boolean removeCard(int cardID, int deckID);
+	int createQuiz (String title, String description, int deckID);
+	
+	boolean addAllStudentsToQuiz (String quizID);
+	boolean addStudentToQuiz (String studentID, String quizID);
+	boolean removeAllStudentsFromQuiz (String quizID);
+	boolean removeStudentFromQuiz (String studentID, String quizID);
+}
