3c22d881acf9b6a4de5a631ad454e7fe7b652172
dmills <dmills@iastate.edu>
2015-02-26 14:20:02 -0600

    added a simple View which reciprocates commands

--- src/flash/card/java/view/Prompt.java
+++ src/flash/card/java/view/Prompt.java
@@ -1,5 +1,31 @@
 package flash.card.java.view;
 
-public class Prompt {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Prompt {
+    public static void main (String args[]) {
+        boolean running;
+        String input;
+        BufferedReader br;
+        
+        running = true;
+        
+        do {
+            try {
+                br = new BufferedReader(new InputStreamReader(System.in));
+
+                while ((input = br.readLine()) != null) {
+                    System.out.println(input);
+                }
+                
+            } catch (IOException io) {
+                // invalid input?
+                io.printStackTrace();
+            }
+        
+        } while (running);
+        
+    }
 }
