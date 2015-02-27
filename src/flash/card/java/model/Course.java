957ab90fd0fce670a5528d5f1cc7c01353fe62f6
Trey Erenberger <treye22@gmail.com>
2015-02-27 11:46:22 -0600

    Added constructors to course model

--- src/flash/card/java/model/Course.java
+++ src/flash/card/java/model/Course.java
@@ -1,7 +1,25 @@
 package flash.card.java.model;
 
+import java.util.HashMap;
+
 import flash.card.java.interfaces.CourseInterface;
 
 public class Course implements CourseInterface {
 
+	private HashMap<String, Student> studentList;
+	private String courseName;
+	
+	public Course (String courseName) {
+		this.studentList = new HashMap<String, Student>();
+		this.courseName = courseName;
+	}
+	
+	public Course (String courseName, HashMap<String, Student> studentList) {
+		this.studentList = studentList;
+		this.courseName = courseName;
+	}
+	
+	public String getCourseName() {
+		return this.courseName;
+	}
 }
